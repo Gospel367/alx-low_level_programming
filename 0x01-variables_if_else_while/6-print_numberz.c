d1048e7ee9ec8966738039ef1f52ca92b0b2220d
Gospel Ferdinand <Ferdinandchinaza@gmail.com>
2022-09-09 16:07:21 -0700

    Add task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
-
-
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base
- * 10 starting from 0, followed by a new line
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
 	for (n = 12; n < 22; n++)
-		printf("%d", n);
-	printf("\n");
+	{
+		putchar(n);
+	}
+	putchar('\n');
 	return (0);
 }
