705d7ecf72c4cf55cfcefd53462324d8d7118b92
Gospel Ferdinand <Ferdinandchinaza@gmail.com>
2022-09-13 19:10:07 -0700

    Add 2-largest_number.c file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 
 /**
@@ -10,21 +12,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else if (c > a && c > b)
-    {
-        largest = c;
-    }
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
-
